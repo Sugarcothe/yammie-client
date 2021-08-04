5542452ab717c1502927071a1881b636e52004e9
Sugarcothe <63057848+Sugarcothe@users.noreply.github.com>
2021-08-04 13:53:06 +0100

    first home done

--- src/components/Home/FirstComponent.js/FirstHome.js
+++ src/components/Home/FirstComponent.js/FirstHome.js
@@ -1,89 +1,95 @@
 import React from 'react';
-import './FirstHome.scss'
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
+import './FirstHome.scss';
+
 
 const FirstHome = () => {
-//   // items
-// state = [
-//   {
-//     icon: 'fas fa-shopping-basket';
-//     item: 'EasyMarket with Yammie';
-//   },
-//   {
-//     icon: 'fas fa-pizza-slice';
-//     item: 'Fast Foods & Drinks';
-//   },
-//   {
-//     icon: 'far fa-money-bill-alt';
-//     item: 'Supermarket';
-//   },
-//   {
-//     icon: 'fas fa-headphones-alt';
-//     item: 'Phones and Accessories';
-//   },
-//   {
-//     icon: 'fas fa-plug';
-//     item: 'Electronics';
-//   },
-//   {
-//     icon: 'fas fa-tshirt';
-//     item: 'Clothes and Shoes';
-//   },
-//   {
-//     icon: 'fas fa-utensils';
-//     item: 'Kitchen & Utensils';
-//   },
-//   {
-//     icon: 'fas fa-desktop';
-//     item: 'Computing & Accessories';
-//   },
-//   {
-//     icon: 'fas fa-child';
-//     item: 'Kids Section';
-//   },
-//   {
-//     icon: 'fas fa-bath';
-//     item: 'Cleaning, Healthy & Beauty';
-//   },
-//   {
-//     icon: 'fas fa-print';
-//     item: 'stationery';
-//   },
-// ]
+  
+  // items
+const items = [
+  {
+    icon: 'fas fa-shopping-basket',
+    item: 'EasyMarket with Yammie',
+  },
+  {
+    icon: 'fas fa-pizza-slice',
+    item: 'Fast Foods & Drinks',
+  },
+  {
+    icon: 'fas fa-cart-arrow-down',
+    item: 'Supermarket',
+  },
+  {
+    icon: 'fas fa-headphones-alt',
+    item: 'Phones and Accessories',
+  },
+  {
+    icon: 'fas fa-plug',
+    item: 'Electronics',
+  },
+  {
+    icon: 'fas fa-tshirt',
+    item: 'Clothes and Shoes',
+  },
+  {
+    icon: 'fas fa-utensils',
+    item: 'Kitchen & Utensils',
+  },
+  {
+    icon: 'fas fa-desktop',
+    item: 'Computing & Accessories',
+  },
+  {
+    icon: 'fas fa-child',
+    item: 'Kids Section',
+  },
+  {
+    icon: 'fas fa-bath',
+    item: 'Cleaning, Healthy & Beauty',
+  },
+  {
+    icon: 'fas fa-print',
+    item: 'stationery',
+  },
+]
 
-//   // center
-// slide = [
-//   {
-//     img: '../img/what.jpg'
-//   },
-//   {
-//     img: '../img/what1.jpg'
-//   },
-//   {
-//     img: '../img/what3.jpg'
-//   },
-//   {
-//     img: '../img/what4.jpg'
-//   },
-// ]
+  // center
+const slides = [
+  {
+    img: 'assets/what.jpeg'
+  },
+  {
+    img: 'assets/what1.jpeg'
+  },
+  {
+    img: 'assets/what3.jpeg'
+  },
+  {
+    img: 'assets/what4.jpeg'
+  },
+]
 
 
 return (
+  
   <div className='container' id='first-home'>
     <div className='sections'>
-
-      <div className='item-card'>
-        <div className='items'>
-          <i class="fas fa-atom"></i>
-          <p>Fashion and Grosseries</p>
-        </div>
+      
+        <div className='item-card'>
+        {items.map((d) => (
+          <div className='items'>
+            <i class={d.icon}></i>
+            <p>{d.item}</p>
+          </div>
+        ))}
       </div>
 
+      
       <div className='slide-card'>
-        <img src='assets/what1.jpeg' alt='this is'/>
+          <div className='ad-covers'>
+            <img src='assets/what4.jpeg' alt='this'/> 
+          </div>
       </div>
-
+    
       <div className='ad-card'>
         <div className='ad-covers'>
           <img src='assets/lady.gif' alt='shoopers'/>
